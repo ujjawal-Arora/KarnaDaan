3fd43e938543831b18a70e2a5b38625e20b769db
Ujjawal arora <ujjawal.arora2004@gmail.com>
2024-08-20 17:01:28 +0530

    new changes are made

--- Daan/server/routes/reqRouter.js
+++ Daan/server/routes/reqRouter.js
@@ -2,6 +2,6 @@ import express  from 'express';
 const app= express();
 const router=express.Router();
 router.get('/',async   (req,res)=>{
-return res.json("hello from request");
+return res.json("hello from");
 })
 export default router;
