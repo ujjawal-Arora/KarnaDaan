6c6c6bea540eb877bc8e4fbbd875812605620d1f
Ujjawal arora <ujjawal.arora2004@gmail.com>
2024-10-13 21:22:48 +0530

    post

--- Daan/server/routes/reqRouter.js
+++ Daan/server/routes/reqRouter.js
@@ -7,15 +7,27 @@ router.post('/add-req',authenticate,addReq);
 
 // router.get("/get-all-req",authenticate,getAllRequests);
 
-router.delete("/delete-req/:reqId",authenticate,deleteReq);
+// router.delete("/delete-req/:reqId",authenticate,deleteReq);
 
-router.put("/update-req/:reqId",authenticate,updateReq);
+// router.put("/update-req/:reqId",authenticate,updateReq);
 
-router.get("/get-allusers-accepted-req",authenticate,getAllUsersAcceptedReq);
+// router.get("/get-allusers-accepted-req",authenticate,getAllUsersAcceptedReq);
 
-router.get("/get-allusers-non-accepted-req",authenticate,getAllUsersNonAcceptedReq);
+// router.get("/get-allusers-non-accepted-req",authenticate,getAllUsersNonAcceptedReq);
 
-router.get("/get-all-non-accepted-req",authenticate,getAllNonAcceptedReq);
+// router.get("/get-all-non-accepted-req",authenticate,getAllNonAcceptedReq);
 
-router.get("/get-all-accepted-req",authenticate,getAllAcceptedReq);
+// router.get("/get-all-accepted-req",authenticate,getAllAcceptedReq);
+
+router.delete("/delete-req/:reqId",deleteReq);
+
+router.put("/update-req/:reqId",updateReq);
+
+router.get("/get-allusers-accepted-req",getAllUsersAcceptedReq);
+
+router.get("/get-allusers-non-accepted-req",getAllUsersNonAcceptedReq);
+
+router.get("/get-all-non-accepted-req",getAllNonAcceptedReq);
+
+router.get("/get-all-accepted-req",getAllAcceptedReq);
 export default router;
